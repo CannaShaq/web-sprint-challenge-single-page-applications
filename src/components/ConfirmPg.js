c5cdec1027f5ad79c32aa7a347c994731588f0b3
CannaShaq <itscoldinwinter2@gmail.com>
2020-08-18 05:27:06 -0700

    confirmPage
    props set up for confirm page,
    need to pass props from post(response data) to conPg component.

--- src/components/ConfirmPg.js
+++ src/components/ConfirmPg.js
@@ -3,7 +3,15 @@ import React from 'react';
 const ConfirmPg = (props) => {
     return(
         <div>
-            Confirmation Pizza was bought! Enjoy !
+            Name: {props.name}<br/>
+            <hr/>
+            Size: {props.size}<br/>
+            Sauce: {props.sauce.value}<br/>
+            Toppings: {props.toppings}<br/>
+            Special Instructions: {props.specInstruct}<br/>
+            {/*Order Number: {props.res.data.id}*/}
+            
+
         </div>
     )
 }
